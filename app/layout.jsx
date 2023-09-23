cd7163d0f1867edf3edb77c494fc07d2241d157d
omJadhav1 <omjadhav0462@gmail.com>
2023-09-23 10:22:31 +0530

    Basic web setup,Home Page,users DB,NextAuth

---
+++ app/layout.jsx
@@ -0,0 +1,26 @@
+import "@styles/global.css";
+import Provider from "@components/Provider";
+import Nav from "@components/Nav";
+export const metadata = {
+  title: "Promptopia",
+  description: "create and share ai prompts",
+};
+const RootLayout = ({ children }) => {
+  return (
+    <html lang="en">
+      <body>
+        <Provider>
+          <div className="main">
+            <div className="gradient"></div>
+          </div>
+          <main className="app">
+            <Nav />
+            {children}
+          </main>
+        </Provider>
+      </body>
+    </html>
+  );
+};
+
+export default RootLayout;
